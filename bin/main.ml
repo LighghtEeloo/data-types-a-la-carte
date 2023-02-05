2a115003f08d81b39e744611382757d5619af824
LighghtEeloo <litiaeeloo@gmail.com>
2023-02-05 13:19:05 -0500

    impl: OCaml first draft

--- bin/main.ml
+++ bin/main.ml
@@ -2,11 +2,7 @@ open Dtalacarte.Ast
 
 let expr : Expr.t = In (Right (Add (In (Left (Val 1)), In (Left (Val 2)))))
 
-let rec eval : Expr.t -> int = function
-  | In (Left (Val n)) -> n
-  | In (Right (Add (a, b))) -> eval a + eval b
-
 let _ =
   print_newline ();
-  Printf.printf "%d" @@ eval expr;
+  Printf.printf "%d" @@ Expr.eval_expr expr;
   print_newline ();
--- lib/Ast.ml
+++ lib/Ast.ml
@@ -1,32 +1,53 @@
 module type FMap = sig
   type 'node t
+
   val fmap : ('a -> 'b) -> 'a t -> 'b t
 end
 
-module Mu (F: FMap) = struct
-  type t = In of (t F.t)
-  let rec cata alg (In(e)) =
-    alg @@ F.fmap (cata alg) e
+module type ImplAlgs = sig
+  type 'node t
+
+  val eval : int t -> int
 end
 
-module Either (U: FMap) (V: FMap) = struct
-  type 'node t =
-    | Left of 'node U.t
-    | Right of 'node V.t
+module type Node = sig
+  include FMap
+  include ImplAlgs with type 'node t := 'node t
+end
+
+module Mu (F : FMap) = struct
+  type t = In of t F.t
+
+  let rec cata alg (In e) = alg @@ F.fmap (cata alg) e
+end
+
+module Either (U : Node) (V : Node) = struct
+  type 'node t = Left of 'node U.t | Right of 'node V.t
+
   let fmap f = function
-    | Left(x) -> Left(U.fmap f x)
-    | Right(x) -> Right(V.fmap f x)
+    | Left x -> Left (U.fmap f x)
+    | Right x -> Right (V.fmap f x)
+
+  let eval = function Left x -> U.eval x | Right x -> V.eval x
 end
 
 module Val = struct
   type _ t = Val of int
-  let fmap _ (Val(x)) = Val(x)
+
+  let fmap _ (Val x) = Val x
+  let eval (Val x) = x
 end
 
 module Add = struct
   type 'node t = Add of 'node * 'node
-  let fmap f (Add(a, b)) = Add(f a, f b)
+
+  let fmap f (Add (a, b)) = Add (f a, f b)
+  let eval (Add (a, b)) = a + b
 end
 
-module Expr = Mu (Either (Val) (Add))
+module Expr = struct
+  module E = Either (Val) (Add)
+  include Mu (E)
 
+  let eval_expr = cata E.eval
+end
